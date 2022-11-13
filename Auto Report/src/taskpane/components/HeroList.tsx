3d17ac32d86a5360c8dc28a94a264860eaabb89a
Zane Hutchison <61723800+zhutcho@users.noreply.github.com>
2022-11-13 19:38:46 +1000

    Added in a list and button

--- Auto Report/src/taskpane/components/HeroList.tsx
+++ Auto Report/src/taskpane/components/HeroList.tsx
@@ -1,31 +1,28 @@
+import { Checkbox, Stack } from "@fluentui/react";
 import * as React from "react";
 
-export interface HeroListItem {
-  icon: string;
-  primaryText: string;
+export interface Section {
+  header: string;
+  text: string;
+  included: boolean;
 }
 
 export interface HeroListProps {
-  message: string;
-  items: HeroListItem[];
+  sections: Section[];
 }
 
-export default class HeroList extends React.Component<HeroListProps> {
-  render() {
-    const { children, items, message } = this.props;
+function HeroList(props: React.PropsWithChildren<HeroListProps>) {
+  const { children, sections } = props;
 
-    const listItems = items.map((item, index) => (
-      <li className="ms-ListItem" key={index}>
-        <i className={`ms-Icon ms-Icon--${item.icon}`}></i>
-        <span className="ms-font-m ms-fontColor-neutralPrimary">{item.primaryText}</span>
-      </li>
-    ));
-    return (
-      <main className="ms-welcome__main">
-        <h2 className="ms-font-xl ms-fontWeight-semilight ms-fontColor-neutralPrimary ms-u-slideUpIn20">{message}</h2>
-        <ul className="ms-List ms-welcome__features ms-u-slideUpIn10">{listItems}</ul>
-        {children}
-      </main>
-    );
-  }
+  const listSections = sections.map((item: Section, index: number) => (
+    <Checkbox className="ms-ListItem" label={item.header} key={index} />
+  ));
+  return (
+    <main>
+      <Stack className="ms-List ms-welcome__features ms-u-slideUpIn10">{listSections}</Stack>
+      {children}
+    </main>
+  );
 }
+
+export default HeroList;
